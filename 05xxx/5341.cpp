f0870f6fde59d355ecede23cc584310fa3269422
Airhood <shinesky0530@gmail.com>
2025-04-03 21:47:22 +0900

    5341

---
+++ 05xxx/5341.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        int n;
+        cin >> n;
+
+        if (n == 0)
+            break;
+
+        cout << n * (n + 1) / 2 << endl;
+    }
+}
